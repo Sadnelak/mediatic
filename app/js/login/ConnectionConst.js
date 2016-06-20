0cb98c4a367b37460446417a0ac16e19360622b4
BenjaminChauvet49FR <benjchauvet@gmail.com>
2016-06-20 13:15:40 +0200

    Avec Index Merge

--- app/js/login/ConnectionConst.js
+++ app/js/login/ConnectionConst.js
@@ -1,7 +1,24 @@
+(function(){
 "use strict";
+
+var basique = "http://192.168.10.12:8090";
+
 angular
 	.module('login')
-	.value('UrlConnection', {basic : "http://192.168.10.27:8090",
-							adherent : "http://192.168.10.27:8090/resource/adherent.recherche",
-							login : "http://192.168.10.27:8090/resource/connexion.login"});
-
+	.value('UrlConnection', {basic : basique,
+							
+							mRecherche : basique + "/resource/media.recherche",//get
+							mRechercheT : basique + "/resource/media.recherche.taille",//get
+							mAccession : basique + "/resource/media.accession",//get
+							mCreation : basique + "/resource/media.creation",//post
+							mModification : basique + "/resource/media.modification",//post
+							aRecherche : basique + "/resource/adherent.recherche",//get
+							aRechercheT : basique + "/resource/adherent.recherche.taille",//get
+							aAccession : basique + "/resource/adherent.accession",//get
+							aCreation : basique + "/resource/adherent.creation",//post
+							aModification : basique + "/resource/adherent.modification",//post
+							eAjout : basique + "/resource/emprunt.ajout",//post
+							cLogin : basique + "/resource/connexion.login",//post
+							cRight : basique + "/resource/connexion.right",});//get
+							
+})()
--- app/js/login/ServiceConnection.js
+++ app/js/login/ServiceConnection.js
@@ -1,31 +1,107 @@
 "use strict";
-angular
-	.module('login')
-	.factory('RequeteLogin', function ($http, $routeParams, UrlConnection) {
-		
-		var promise = $http.get(UrlConnection.adherent).then(function(resultat){
-			console.log(resultat.data)
-			return resultat.data;
-		}, function (){
-			console.error('error');
-			return[];
+angular.module('login').factory(
+		'RequeteLogin',
+		function($http, $routeParams, UrlConnection) {
+
+			
+			var s = {}
+			s.getCRight = function() {
+				var promise = $http.get(UrlConnection.cRight).then(
+						function(resultat) {
+							console.log(resultat.data)
+							return promise;
+						}, function() {
+							console.error('error requete login get');
+							return [];
+						});
+
+			};
+			s.postCLogin = function(monUser) {
+
+				var promise2 = $http.post(UrlConnection.cLogin, monUser).then(
+						function(resultat2) {
+							console.log("result.data : ",resultat2.data);
+							console.log("result : ",resultat2);
+							return true;
+						}, function() {
+							console.error('error requete login post');
+							return false;
+						});
+				return promise2;
+			}
+			
+			
+			
+			
+			return s;
 		});
-		
-		
-		
-		var s = {}	
-		s.getList = function(){
-			return promise;
-		};
-		s.postUser = function(monUser){
-			var promise2 = $http.post(UrlConnection.login, monUser).then(function(resultat2){
-				console.log(resultat2.data);
-				console.log(resultat2);
-				return resultat2;
-			}, function (){
-				console.error('error');
-				return[];
-			});
-		}
-		return s;
-	});
+
+angular.module('media').factory(
+		'RequeteMedia',
+		function($http, $routeParams, UrlConnection) {
+
+			var s = {};
+			
+			s.getMRecherche = function() {
+				var promise = $http.get(UrlConnection.mRecherche).then(
+						function(resultat) {
+							console.log(resultat.data)
+							return resultat.data;
+						}, function() {
+							console.error('error');
+							return [];
+						});
+
+			};
+
+			s.getMRechercheT = function() {
+				var promise = $http.get(UrlConnection.mRechercheT).then(
+						function(resultat) {
+							console.log(resultat.data)
+							return resultat.data;
+						}, function() {
+							console.error('error');
+							return [];
+						});
+
+			};
+			
+			s.getMAccession = function() {
+				var promise = $http.get(UrlConnection.mAccession).then(
+						function(resultat) {
+							console.log(resultat.data)
+							return resultat.data;
+						}, function() {
+							console.error('error');
+							return [];
+						});
+
+			};
+			
+			s.postMCreation = function(monMedia) {
+				var promise2 = $http.post(UrlConnection.mCreation, monMedia)
+						.then(function(resultat2) {
+							console.log(resultat2.data);
+							console.log(resultat2);
+							return resultat2;
+						}, function() {
+							console.error('error');
+							return [];
+						});
+			}
+			
+			s.postMModification = function(monMedia) {
+				var promise2 = $http.post(UrlConnection.mCreation, monMedia)
+						.then(function(resultat2) {
+							console.log(resultat2.data);
+							console.log(resultat2);
+							return resultat2;
+						}, function() {
+							console.error('error');
+							return [];
+						});
+			}
+	
+			
+			return s;
+		});
