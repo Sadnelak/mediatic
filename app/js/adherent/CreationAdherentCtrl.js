adc69c5f43f77194b7fe99b71e945468321cf3a3
Félicien Clarté <clartefelicien@hotmail.com>
2016-06-21 11:59:04 +0200

    1st step ajout adherent

--- app/js/adherent/CreationAdherentCtrl.js
+++ app/js/adherent/CreationAdherentCtrl.js
@@ -1,10 +1,37 @@
+'use strict';
+
 angular
 		.module('adherent')
-		.controller('CreationAdherentCtrl', function(){
+		.controller('CreationAdherentCtrl', function($scope){
 			
 			
-			ajoutAdherent = function(){
-				
+			//$scope.adherent={};
+
+
+			$scope.ajoutAdherent = function(){
+				console.log($scope.adherent);
 			};
 			
+
+			$scope.calculAge = function(){
+				if ($scope.adherent != undefined && $scope.adherent.dateNaissance!= undefined &&
+				 $scope.adherent.dateNaissance.annee!=undefined && $scope.adherent.dateNaissance.mois!=undefined && $scope.adherent.dateNaissance.jour!=undefined){
+
+					var dateNaissance = new Date($scope.adherent.dateNaissance.annee,$scope.adherent.dateNaissance.mois-1,$scope.adherent.dateNaissance.jour);
+					
+					var age = ((new Date().getTime() - dateNaissance.getTime()) / 31536000000).toFixed(0);
+
+
+					console.log(age);
+					$scope.adherent.age=age;
+				}
+			};
+			
+			$scope.verifJour = function (jour){
+				if (jour<0 || jour >31){
+					return jour=undefined;
+				}
+			};
+
+
 		});
