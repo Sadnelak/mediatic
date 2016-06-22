433ffa59a91062240bcf4379459fb2ca96229f07
Félicien Clarté <clartefelicien@hotmail.com>
2016-06-22 10:55:17 +0200

    Creation Adherent juste avant de faire le post

--- app/js/adherent/CreationAdherentCtrl.js
+++ app/js/adherent/CreationAdherentCtrl.js
@@ -5,11 +5,27 @@ angular
 .controller('CreationAdherentCtrl', function($scope,$location,ServiceAuth){
 
 
-			//$scope.adherent={};
-
+			$scope.adherent={};
+			$scope.adherent.cotisation={};
 
 			$scope.ajoutAdherent = function(){
 				console.log($scope.adherent);
+				var champsARemplir = undefined;
+				if ($scope.adherent.nom===undefined || $scope.adherent.nom==='') {
+					champsARemplir=champsARemplir+"le nom, "
+				}
+				if ($scope.adherent.prenom===undefined || $scope.adherent.prenom==='') {
+					champsARemplir=champsARemplir+"le prenom, "
+				}
+				if ($scope.adherent.date_naissance===undefined || $scope.adherent.date_naissance==='') {
+					champsARemplir=champsARemplir+"la date de naissance, "
+				}
+				if ($scope.adherent.email===undefined || $scope.adherent.email==='') {
+					champsARemplir=champsARemplir+"l'email, "
+				}
+
+
+
 			};
 			
 
@@ -24,7 +40,7 @@ angular
 
 						var age = ((new Date().getTime() - dateNaissance.getTime()) / 31536000000).toFixed(0);
 
-
+						$scope.adherent.date_naissance=dateNaissance;
 						console.log(age);
 						$scope.adherent.age=age;
 
@@ -34,6 +50,32 @@ angular
 
 			};
 			
+
+
+			$scope.calculFinAbonnement = function(){
+				if ($scope.adherent != undefined && $scope.adherent.dateCotisation!= undefined){
+
+					$scope.verif($scope.adherent.dateCotisation);
+
+					if ($scope.adherent.dateCotisation.annee!=undefined && $scope.adherent.dateCotisation.mois!=undefined && $scope.adherent.dateCotisation.jour!=undefined){
+
+						var dateCotisation = new Date($scope.adherent.dateCotisation.annee,$scope.adherent.dateCotisation.mois-1,$scope.adherent.dateCotisation.jour);
+
+						var finAbonnement = new Date($scope.adherent.dateCotisation.annee +1,$scope.adherent.dateCotisation.mois-1,$scope.adherent.dateCotisation.jour);
+
+						$scope.adherent.cotisation.debut=dateCotisation;
+						$scope.adherent.cotisation.fin=finAbonnement;
+
+						console.log(finAbonnement);
+						$scope.adherent.dateFinAbonnement=finAbonnement.getDate() +"-"+(finAbonnement.getMonth()+1) +"-"+finAbonnement.getFullYear();
+
+
+					}
+				}
+
+			};
+
+
 			$scope.verif = function (date){
 				date.jour=parseInt(date.jour);
 				date.mois=parseInt(date.mois);
